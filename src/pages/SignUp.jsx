ccd0c37a5faaab466153f0c913253cdb735ada2a
itsaliataie <aataie.b1t@gmail.com>
2022-09-24 19:51:55 -0400

    Login & Signup & PrivateRoute

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,8 @@
 import {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
 import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import {setDoc, doc, serverTimestamp} from 'firebase/firestore'
 import { db } from '../firebase.config'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
@@ -36,9 +38,16 @@ function SingUp() {
       updateProfile(auth.currentUser, {
         displayName: name
       })
+
+      const formDataCopy = {...formData}
+      delete formDataCopy.password
+      formDataCopy.serverTimestamp = serverTimestamp()
+
+      await setDoc(doc(db, 'users', user.uid), formDataCopy)
+
       navigate('/')
     } catch (error) {
-      console.log(error)
+      toast.error('Something went wrong with the registration!')
     }
   }
 
--- src/pages/SingIn.jsx
+++ src/pages/SingIn.jsx
@@ -1,5 +1,7 @@
 import {useState} from 'react'
+import { toast, Toast } from 'react-toastify'
 import {Link, useNavigate} from 'react-router-dom'
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -20,13 +22,29 @@ function SingIn() {
     }))
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)
+
+      if(userCredential.user) {
+        navigate('/')
+      }
+    } catch (error) {
+      toast.error('Username / Password not found')
+    }
+  }
+
   return (
     <>
       <div className="pageContainer">
         <header>
           <p className="pageHeader">Welcome Back!</p>
         </header>
-        <form>
+        <form onSubmit={onSubmit}>
           <input type="email" className='emailInput' placeholder='Email' id='email' value={email} onChange={onChange} />
           <div className="passwordInputDiv">
             <input type={showPassword ? 'text' : 'password'} className='passwordInput' placeholder='Password' id='password' value={password} onChange={onChange} />
